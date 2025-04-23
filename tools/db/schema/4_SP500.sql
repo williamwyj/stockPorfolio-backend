2cf39f7aa5608169a9d0f0c6b83ca200ab3802ec
William Wang <williamwyj1@gmail.com>
2025-04-23 16:32:14 -0700

    added sqlquery table and first row of data

--- tools/db/schema/4_SP500.sql
+++ tools/db/schema/4_SP500.sql
@@ -1,12 +1,12 @@
 DROP TABLE IF EXISTS SP500;
 CREATE TABLE IF NOT EXISTS SP500(
-"Date" TEXT,
-"Ticker" TEXT,
-"Open" REAL,
-"High" REAL,
-"Low" REAL,
-"Close" REAL,
-"Adjusted" REAL,
-"Returns" TEXT,
-"Volume" REAL
+"date" DATE,
+"ticker" TEXT,
+"open" REAL,
+"high" REAL,
+"low" REAL,
+"close" REAL,
+"adjusted" REAL,
+"returns" TEXT,
+"volume" REAL
 );
---
+++ tools/db/schema/5_sqlquery.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS sqlquery;
+CREATE TABLE IF NOT EXISTS sqlquery(
+"id" SMALLINT,
+"title" TEXT,
+"description" TEXT,
+"query" TEXT
+);
